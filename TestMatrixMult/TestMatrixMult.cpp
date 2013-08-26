b43d6ab47647870d222d8466034ca8d7e6eeee75
shobomaru <shobomaru@gmail.com>
2013-08-27 00:21:26 +0900

    Add AVX2 transpose.

--- TestMatrixMult/TestMatrixMult.cpp
+++ TestMatrixMult/TestMatrixMult.cpp
@@ -150,10 +150,10 @@ static void NOINLINE mulX4( const __m128 *v1, const __m128 *v2, __m128 *vout )
 
 static void NOINLINE mulX8( const __m256 *v1, const __m256 *v2, __m256 *vout )
 {
-    const int ALIGN32 p1[ 8 ] = { 0, 0, 0, 0, 1, 1, 1, 1 };
-    const int ALIGN32 p2[ 8 ] = { 2, 2, 2, 2, 3, 3, 3, 3 };
-    const int ALIGN32 p3[ 8 ] = { 4, 4, 4, 4, 5, 5, 5, 5 };
-    const int ALIGN32 p4[ 8 ] = { 6, 6, 6, 6, 7, 7, 7, 7 };
+    static const int ALIGN32 p1[ 8 ] = { 0, 0, 0, 0, 1, 1, 1, 1 };
+    static const int ALIGN32 p2[ 8 ] = { 2, 2, 2, 2, 3, 3, 3, 3 };
+    static const int ALIGN32 p3[ 8 ] = { 4, 4, 4, 4, 5, 5, 5, 5 };
+    static const int ALIGN32 p4[ 8 ] = { 6, 6, 6, 6, 7, 7, 7, 7 };
     const __m256i perm1 = _mm256_load_si256( reinterpret_cast< const __m256i* >( p1 ) );
     const __m256i perm2 = _mm256_load_si256( reinterpret_cast< const __m256i* >( p2 ) );
     const __m256i perm3 = _mm256_load_si256( reinterpret_cast< const __m256i* >( p3 ) );
@@ -200,7 +200,7 @@ static void NOINLINE transposeX4( const __m128 *v1, __m128 *vout )
 
 static void NOINLINE transposeX8( const __m256 *v1, __m256 *vout )
 {
-    // Maybe slow than SSE
+#if 0 // AVX1
     __m256 a0 = _mm256_unpacklo_ps( v1[ 0 ], v1[ 1 ] );
     __m256 a1 = _mm256_unpackhi_ps( v1[ 0 ], v1[ 1 ] );
     __m256 b0 = _mm256_permute2f128_ps( a0, a1, _MM_SHUFFLE( 0, 2, 0, 0 ) );
@@ -209,6 +209,16 @@ static void NOINLINE transposeX8( const __m256 *v1, __m256 *vout )
     __m256 c1 = _mm256_unpackhi_ps( b0, b1 );
     vout[ 0 ] = _mm256_permute2f128_ps( c0, c1, _MM_SHUFFLE( 0, 2, 0, 0 ) );
     vout[ 1 ] = _mm256_permute2f128_ps( c0, c1, _MM_SHUFFLE( 0, 3, 0, 1 ) );
+#else // AVX2
+    static const int ALIGN32 p1[ 8 ] = { 0, 4, 2, 6, 1, 5, 3, 7 };
+    static const int ALIGN32 p2[ 8 ] = { 2, 6, 0, 4, 3, 7, 1, 5 };
+    const __m256i perm1 = _mm256_load_si256( reinterpret_cast< const __m256i* >( p1 ) );
+    const __m256i perm2 = _mm256_load_si256( reinterpret_cast< const __m256i* >( p2 ) );
+    __m256 a0 = _mm256_permutevar8x32_ps( v1[ 0 ], perm1 );
+    __m256 a1 = _mm256_permutevar8x32_ps( v1[ 1 ], perm2 );
+    vout[ 0 ] = _mm256_blend_ps( a0, a1, 0xCC );
+    vout[ 1 ] = _mm256_shuffle_ps( a0, a1, 0x4E );
+#endif
 }
 
 int main(int argc, const char * argv[])
